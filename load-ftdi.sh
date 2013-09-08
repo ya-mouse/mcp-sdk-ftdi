5d0b3a47b9fc15878078cebceddaa928c9b8ebce
Anton D. Kachalov <mouse@yandex-team.ru>
2013-09-08 18:53:26 +0400

    Add x86/x86_64 variants for libftd2xx Signed-off-by: Anton D. Kachalov <mouse@yandex-team.ru>

--- load-ftdi.sh
+++ load-ftdi.sh
@@ -1,12 +1,29 @@
 #!/bin/sh -e
 
+cwd="$(readlink -f ${0%/*})"
+
 sigstop()
 {
     echo "Interrupted."
     trap - EXIT TERM KILL INT QUIT
-    rm -f $PWD/lock-ftdi-B.lck
+    rm -f $cwd/lock-ftdi-B.lck
     kill -9 $lckpid 2>/dev/null ||:
 }
+
+case "$(uname -m)" in
+i[3456]86)
+    lib=lib
+    ;;
+x86_64)
+    lib=lib64
+    ;;
+*)
+    echo "Unsupported architecture: $(uname -r)"
+    exit 1
+esac
+
+rm -f $cwd/libftd2xx.so.1.1.0
+ln -s $cwd/$lib/libftd2xx.so.1.1.0 $cwd/libftd2xx.so.1.1.0
 
 trap sigstop EXIT TERM KILL INT QUIT
 
@@ -18,13 +35,13 @@ trap sigstop EXIT TERM KILL INT QUIT
 cnt="$(ls -1 /dev/ttyUSB* 2>/dev/null | wc -l || echo 0)"
 
 # Lock PicoTAP USB channel
-rm -f $PWD/lock-ftdi-B.lck
-LD_LIBRARY_PATH=$PWD python lock-ftdi-B.py &
+rm -f $cwd/lock-ftdi-B.lck
+LD_LIBRARY_PATH=$cwd python lock-ftdi-B.py &
 lckpid=$!
 
 # Wait for lock file
 retry=0
-while [ ! -e $PWD/lock-ftdi-B.lck -a $retry -lt 10 ]; do
+while [ ! -e $cwd/lock-ftdi-B.lck -a $retry -lt 10 ]; do
     sleep 1
     retry=$((retry+1))
 done
@@ -34,7 +51,7 @@ if [ $retry = 10 ]; then
     exit 1
 fi
 
-rm -f $PWD/lock-ftdi-B.lck
+rm -f $cwd/lock-ftdi-B.lck
 
 # Create ttyUSBx for USB serial channel
 modprobe ftdi_sio
